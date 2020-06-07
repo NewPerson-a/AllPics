4d73e57a1044a5efdad0c6a2a38a3669bb34fbcc
NewPerson-a <66472969+NewPerson-a@users.noreply.github.com>
2020-06-07 12:59:55 +0300

    Add files via upload
    Version 3.2
    Timer now works correctly
    Added ability to adjust amount of steps per pixel, which allows regulating quality and speed
    Just a little bit optimization

--- README.txt
+++ README.txt
@@ -1,17 +1,19 @@
 ABOUT
-AllPics is a simple SDL-based program that can show you every image possible if given enough time. The problem is it requires a lot of time. Still, it works. When you start the aplication it asks you for width and height of the window. Once you entered it creates a window and starts to change pixels.
+AllPics is a simple SDL-based (https://libsdl.org/) program that can show you every image possible if given enough time. The problem is it requires a lot of time. Still, it works. When you start the application it asks you for width and height of the window, and for amount of steps per color. Once you entered this data it creates a window and starts changing pixels.
 
 HOW TO USE
-SPACE ‒ pause/unpace; 
+SPACE ‒ pause/unpause;
 S ‒ save;
-L - load;
-U - turn on/off window update. Works much fuster when turned off;
-H - turn on/of display of ammount of pixel changes per second
-Q - quit
+L ‒ load;
+U ‒ turn on/off window update. Works much faster when turned off. Actually it is recommended to keep it off most of time and turn on only if you want to see current progress;
+H ‒ turn on/of display of amount of pixel changes per second
+Q ‒ quit
+A ‒ adjust amount of steps per color. The higher this number is the more detailed image can be generated and the slower program works.
 
-Note: you will need a terminal or console to run this application. This will be changed in future.
+
+Note: you will need a terminal or a console to run this application. This will be changed in future.
 Second note: this program should work under any desktop platform, but was only tested under debian linux.
 Yet another note: for some reason the program doesn't quit when paused, no matter how you try (even "sudo kill" didn't work), but it remembers that you wanted to quit and does so once unpaused.
 
-ALGORITM
+ALGORITHM
 Each pixel consists of five variables: x-position, y-position, red, green, blue. At first we increase the red value a bit. Once it reaches 255 we set it back to zero and increase the green value. Once green reaches 255, we set it to 0 and increase blue value. And when it reaches 255, we set it to 0 and increase the red value of the next pixel. And so on. The program finishes when the number of next pixel is more than amount of pixels.
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ S to save
 L to load
 H to display ammount of operations per second
 U to turn on/off screen update. When turned off works much faster
+A to adjust iterations per color. The higher the number the more detailed image can be generated, but speed drops rapidly
 
 Known issue: doesn't quit when paused
 */
@@ -54,35 +55,60 @@ void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 
 int main (int argc, char** argv)
 {
-  int x = 0, y = 0, step = 51, counter = 0, A = 0;   //recomended steps: 1(255 iterations per color); 3(85); 5(51); 15(17); 17(15); 51(5); 85(3); 255(1)
-  bool stop = false, pause = false, update = true, how_fast = false;
+  int x = 0, y = 0, counter = 0, A = 0, it_p_color = 0, how_fast_count = 0;
+  bool stop = false, pause = false, update = true, how_fast = false, debug = false;
   string filename;
-  fstream f;
+  fstream f, debug_f;
+
+  if (argc > 1)
+  {
+    debug = true;
+    debug_f.open ("DEBUGFILE.debug", ios::out);
+    if (!debug_f.is_open ())
+    {
+      cout << "Failed to open debug file. Continuing without debug." << endl;
+      debug = false;
+    }
+  }
 
   cout << "X = ";
   cin >> x;
   cout << "Y = ";
   cin >> y;
+  cout << endl << "The higher this number the higer quality of the image and the slower its generation;" << endl;
+  cout << "Iterations per color (min: 1; max: 255): ";
+  cin >> it_p_color;
+  if (it_p_color > 255)
+  {
+    cout << "Entered number is greater than 255. It will be set to 255." << endl;
+    it_p_color = 255;
+  }
+
+  if (it_p_color < 1)
+  {
+    cout << "Entered number is less than 1. It will be set to 1." << endl;
+    it_p_color = 1;
+  }
+  float step = 255.0 / (it_p_color * 1.0);
 
   int xy = x * y;
-  vector <int> r (xy, 0);
-  vector <int> g (xy, 0);
-  vector <int> b (xy, 0);
+  vector <float> r (xy, 0.0);
+  vector <float> g (xy, 0.0);
+  vector <float> b (xy, 0.0);
 
   Uint32 color = 0, begin_tick = 0;
-  int how_fast_count = 0;
   SDL_Init (SDL_INIT_VIDEO);
   SDL_Event Event;
   SDL_KeyboardEvent KbrdEvent;
-  SDL_Surface* Surface;
   SDL_Window* MainWindow = SDL_CreateWindow (
-                                            "AllPics V3.1",
+                                            "AllPics V3.2",
                                             SDL_WINDOWPOS_CENTERED,
                                             SDL_WINDOWPOS_CENTERED,
                                             x,
                                             y,
                                             0
                                             );
+  SDL_Surface* Surface = SDL_GetWindowSurface (MainWindow);
   begin_tick = SDL_GetTicks ();
   while (!stop)
   {
@@ -155,6 +181,7 @@ int main (int argc, char** argv)
               {
                 f << b [i] << ' ';
               }
+              f << endl << it_p_color << endl;
               cout << "Saved." << endl;
             } else
             {
@@ -200,8 +227,11 @@ int main (int argc, char** argv)
                   f >> A;
                   b.push_back (A);
                 }
+                f >> it_p_color;
+                f.close ();
+                step = 255.0 / (it_p_color * 1.0);
                 MainWindow = SDL_CreateWindow (
-                                                "AllPics V3.1",
+                                                "AllPics V3.2",
                                                 SDL_WINDOWPOS_CENTERED,
                                                 SDL_WINDOWPOS_CENTERED,
                                                 x,
@@ -217,13 +247,13 @@ int main (int argc, char** argv)
                 }
                 SDL_UnlockSurface (Surface);
                 SDL_UpdateWindowSurface (MainWindow);
+
                 pause = true;
                 cout << "Loaded and paused." << endl;
               } else
               {
                 cout << "Error: couldn't open file." << endl;
               }
-              f.close ();
               break;
 
 
@@ -250,7 +280,26 @@ int main (int argc, char** argv)
               {
                 cout << "Delay counter disabled." << endl;
               }
+              break;
 
+
+
+            case SDL_SCANCODE_A:
+              cout << "Iterations per color (min: 1; max: 255): ";
+              cin >> it_p_color;
+              if (it_p_color > 255)
+              {
+                cout << "Entered number is greater than 255. It will be set to 255." << endl;
+                it_p_color = 255;
+              }
+
+              if (it_p_color < 1)
+              {
+                cout << "Entered number is less than 1. It will be set to 1." << endl;
+                it_p_color = 1;
+              }
+              step = 255.0 / (it_p_color * 1.0);
+              break;
         }
       }
 
@@ -270,20 +319,20 @@ int main (int argc, char** argv)
       goto program_end;
     } else
     {
-      if (r[counter] >= 255)
+      if (r[counter] >= 255.0)
       {
-        r[counter] = 0;
-        if (g[counter] >= 255)
+        r[counter] = 0.0;
+        if (g[counter] >= 255.0)
         {
-          g[counter] = 0;
-          if (b[counter] >= 255)
+          g[counter] = 0.0;
+          if (b[counter] >= 255.0)
           {
-            b[counter] = 0;
+            b[counter] = 0.0;
             counter += 1;
           } else
           {
             b[counter] += step;
-            counter = 0;
+            counter = 0.0;
           }
         } else
         {
@@ -297,8 +346,8 @@ int main (int argc, char** argv)
       }
     }
 
-      Surface = SDL_GetWindowSurface (MainWindow);
-      color = SDL_MapRGB (Surface->format, r[counter], g[counter], b[counter]);
+      int R = r[counter], G = g[counter], B = b[counter];
+      color = SDL_MapRGB (Surface->format, R, G, B);
       SDL_LockSurface (Surface);
       putpixel (Surface, (counter % x), (counter / x), color);
       SDL_UnlockSurface (Surface);
@@ -307,8 +356,29 @@ int main (int argc, char** argv)
         SDL_UpdateWindowSurface (MainWindow);
       }
 
-    //cout << "Loop." << endl;
-    //SDL_Delay (100);
+    if (debug)
+    {
+      for (int i = 0; i < xy; i++)
+      {
+        debug_f << r[i] << "  ";
+      }
+
+      debug_f << endl;
+
+      for (int i = 0; i < xy; i++)
+      {
+        debug_f << g[i] << "  ";
+      }
+
+      debug_f << endl;
+
+      for (int i = 0; i < xy; i++)
+      {
+        debug_f << b[i] << "  ";
+      }
+
+      debug_f << endl << counter << endl << endl << endl;
+    }
   }
 
 
@@ -318,12 +388,17 @@ int main (int argc, char** argv)
   Uint32 worked_for = SDL_GetTicks ();
   SDL_DestroyWindow (MainWindow);
   SDL_Quit ();
+  if (debug)
+  {
+    debug_f.close ();
+  }
   cout << "done." << endl;
 
   int days = 0, hours = 0, minutes = 0, seconds = 0;
 
   cout << "Program worked for";
 
+  /*
   days = worked_for / 8640000;
   if (days)
   {
@@ -352,6 +427,40 @@ int main (int argc, char** argv)
   } else
   {
     cout << endl;
+  }*/
+
+  seconds = worked_for / 1000;
+  if (seconds > 59)
+  {
+    minutes = seconds / 60;
+    seconds %= 60;
   }
+
+  if (minutes > 59)
+  {
+    hours = minutes / 60;
+    minutes %= 60;
+  }
+
+  if (hours > 23)
+  {
+    days = hours / 24;
+    hours %= 24;
+  }
+
+
+  if (days)
+  {
+    cout << ' ' << days << 'd';
+  }
+  if (hours)
+  {
+    cout << ' ' << hours << 'h';
+  }
+  if (minutes)
+  {
+    cout << ' ' << minutes << 'm';
+  }
+  cout << ' ' << seconds << 's' << endl;
   return 0;
 }
